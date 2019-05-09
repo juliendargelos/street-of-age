81e8cf6e691bb17f3b0984be09ae056a7ea44e11
Omar Jbara <omar.jbara2@gmail.com>
2019-05-09 18:08:08 +0200

    - Added missing type in shared 'Sprite' and 'Body' types

--- sources/shared/src/@types/level.d.ts
+++ sources/shared/src/@types/level.d.ts
@@ -7,12 +7,28 @@ export type LevelBackground = {
   to: Color
 }
 
+export type Sprites = { [layer: string]: Sprite[] }
+
+export interface Sprite {
+  id?: string
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  texture: string,
+  frame?: string
+}
+
 export interface Body {
   x: number,
   y: number,
   width: number,
   height: number,
-  texture?: string
+  texture?: string,
+  pivot: {
+    x: number,
+    y: number
+  }
 }
 
 export interface Level {
