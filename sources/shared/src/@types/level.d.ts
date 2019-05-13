3d31db4568706946980a50298575a7a7fd3d30a2
Omar Jbara <omar.jbara2@gmail.com>
2019-05-13 03:53:09 +0200

    - Fixes after rebase

--- sources/shared/src/@types/level.d.ts
+++ sources/shared/src/@types/level.d.ts
@@ -7,18 +7,6 @@ export type LevelBackground = {
   to: Color
 }
 
-export type Sprites = { [layer: string]: Sprite[] }
-
-export interface Sprite {
-  id?: string
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-  texture: string,
-  frame?: string
-}
-
 export interface Body {
   x: number,
   y: number,
