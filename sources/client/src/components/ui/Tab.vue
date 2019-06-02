8ff2430c7a654995e1bcfac26d3dac017736b375
Omar Jbara <omar.jbara2@gmail.com>
2019-06-03 01:59:02 +0200

    - Added a generic tab compound component which allow to compose our layout by non restricting its usage to some predefined layout. <TabList>, <Tab> and <TabPanel> are used together

---
+++ sources/client/src/components/ui/Tab.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tab" @click="onClickHandler">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="sass">
+.tab
+  font-size: 20px
+</style>
+<script lang="ts">
+import { Vue, Component, Inject, Prop } from 'vue-property-decorator'
+
+@Component<Tab>({})
+export default class Tab extends Vue {
+  @Prop({ type: String, required: true }) readonly id!: string
+  @Inject('changeTab') readonly changeTab!: (newId: string) => void
+
+  public onClickHandler (): void {
+    this.changeTab(this.id)
+  }
+}
+</script>
---
+++ sources/client/src/components/ui/TabList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="tabs-list" :key="activeTabId">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="sass">
+
+</style>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component<TabList>({
+  provide () {
+    return Object.defineProperties({}, {
+      activeTabId: { enumerable: true, get: () => this.activeTabId },
+      changeTab: { enumerable: true, get: () => this.changeTab }
+    })
+  }
+})
+export default class TabList extends Vue {
+    public activeTabId: string = 'settings'
+
+    public changeTab (newId: string): void {
+      this.activeTabId = newId
+    }
+}
+</script>
