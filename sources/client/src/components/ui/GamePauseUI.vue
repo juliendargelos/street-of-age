505a7c377737943cfdc831c5c9cd9f1363cc15d7
Omar Jbara <omar.jbara2@gmail.com>
2019-06-03 02:20:09 +0200

    - WIP GamePauseUI component : added tabs and text for credit tab

--- sources/client/src/components/ui/GamePauseUI.vue
+++ sources/client/src/components/ui/GamePauseUI.vue
@@ -11,7 +11,26 @@
       </div>
     </nav>
     <main>
-      <h2>Content</h2>
+      <TabList defaultTab="credits">
+        <div class="tab-panel-tabs">
+          <Tab class="tab-panel-tabs__title" id="settings">Paramètres</Tab>
+          <Tab class="tab-panel-tabs__title" id="help">Aides</Tab>
+          <Tab class="tab-panel-tabs__title" id="credits">Crédit</Tab>
+        </div>
+        <div class="tab-panel-view">
+          <TabPanel forTab="settings">
+            <h2>Contenu des paramètres</h2>
+          </TabPanel>
+          <TabPanel forTab="help">
+            <h2>Contenu de l'aide</h2>
+          </TabPanel>
+          <TabPanel class="tab-panel-view--credits" forTab="credits">
+            <p>Un projet réalisé par <em>Iris Mangin</em>, <em>Fany Thourain</em>, <em>Bastien Lepecheur</em>,
+              <em>Julien Dargelos</em> et <em>Omar Jbara</em> dans le cadre de l'école Gobelins.</p>
+            <small class="copyright">Tous droits resérvés, 2019</small>
+          </TabPanel>
+        </div>
+      </TabList>
     </main>
   </div>
 </template>
@@ -25,6 +44,15 @@
   width: 100%
   height: 100%
   pointer-events: all
+  & p
+    font-weight: 300
+    font-size: 16px
+    line-height: 24px
+  & .copyright
+    display: inline-block
+    margin-top: 40px
+  & em
+    font-weight: 500
   & nav
     flex: 1
     background: transparentize($blue, 0.45)
@@ -52,15 +80,37 @@
       padding-bottom: 30px
   & main
     flex: 2
+    & .tabs-list
+      height: 100%
+      display: flex
+      flex-direction: column
+    & .tab-panel-tabs
+      display: flex
+      justify-content: space-evenly
+      padding: 40px 20px 20px 20px
+      &__title
+        opacity: 0.3
+        &.selected
+          opacity: 1
+        &:hover
+          opacity: 1
+          cursor: pointer
+    & .tab-panel-view
+      padding: 20px
+      &--credits
+        margin-top: 40px
 
 </style>
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 import Logo from '@/components/ui/icons/Logo.vue'
+import TabList from '@/components/ui/TabList.vue'
+import Tab from '@/components/ui/Tab.vue'
+import TabPanel from '@/components/ui/TabPanel.vue'
 
-@Component({
-  components: { Logo }
-})
+  @Component({
+    components: { TabPanel, Tab, TabList, Logo }
+  })
 export default class GamePauseUI extends Vue {
 }
 </script>
