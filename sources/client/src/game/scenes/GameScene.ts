7cf5e526cc3be6cb6e1198d81a1232419a1d821a
Omar Jbara <omar.jbara2@gmail.com>
2019-06-04 04:25:50 +0200

    - Set doting granny as default in debug game

--- sources/client/src/game/scenes/GameScene.ts
+++ sources/client/src/game/scenes/GameScene.ts
@@ -19,7 +19,7 @@ export class GameScene extends BaseScene {
     super.create()
     this.character = new Character({
       scene: this,
-      kind: CharacterKind.MrMuscle,
+      kind: 'doting-granny' as CharacterKind,
       x: 120,
       y: 200
     })
