c1893409f272e9924140335025af8bc29cd0cff7
Omar Jbara <omar.jbara2@gmail.com>
2019-06-10 18:19:35 +0200

    - Fixed type error

--- sources/server/src/controllers/GameController.ts
+++ sources/server/src/controllers/GameController.ts
@@ -8,7 +8,7 @@ import { Game } from '../entities/Game'
 export class GameController extends Controller {
   private player: Player
   private game!: Game
-  private interval: number = null
+  private interval: NodeJS.Timeout = null
 
   constructor(socket: SocketIO.Socket) {
     super(socket, [
